0d6ff6beea8b2421253b4a99b911af414242883e
Bryce Hebert <brycehebert@users.noreply.github.com>
2021-12-28 22:06:21 -0600

    Preparing for deploy on heroku

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const cors = require("cors");
 const fetch = require("node-fetch");
 
-app.use(cors({methods: "POST"}));
+app.use(cors({ methods: "POST" }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./client/build/index.html"));
+});
 
 app.post("/apod", (req, res) => {
   const { url } = req.body;
